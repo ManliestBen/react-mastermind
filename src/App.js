3b8fbab9f9da45ee86e0d45a9aa0a725239e768f
ManliestBen <benjamintmanley@gmail.com>
2019-09-25 16:53:24 -0500

    add props for guessing and score button

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     super();
     this.state = {
       selColorIdx: 0,
-      guesses: [],
+      guesses: [this.getNewGuess(), this.getNewGuess()],
       code: this.genCode()
     };
   }
@@ -21,10 +21,21 @@ class App extends Component {
     return new Array(4).fill().map(dummy => Math.floor(Math.random() * colors.length));
   };
 
+  getNewGuess() {
+    return {
+      // code: [null, null, null, null],
+      code: [0, 1, 2, 3],
+      score: {
+        perfect: 0,
+        almost: 0
+      }
+    };
+  }
+
   render() {
     return (
       <div className="App">
-          <button onClick={() => this.setState((state) => ({selColorIdx: ++state.selColorIdx % 4}))}>;
+          <button onClick={() => this.setState((state) => ({selColorIdx: ++state.selColorIdx % 4}))}>
             Next Color
           </button>
         Selected Color: {colors[this.state.selColorIdx]}
