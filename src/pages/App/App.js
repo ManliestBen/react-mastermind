2e0ac9579773f6e31f402241e900f8ef7ee14955
ManliestBen <benjamintmanley@gmail.com>
2019-10-01 09:44:10 -0500

    add difficulty page elements

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import GamePage from '../GamePage/GamePage';
-import SettingsPage from '../SettingsPage/SettingsPage';
+import GamePage from '../../pages/GamePage/GamePage';
 import { Route, Switch } from 'react-router-dom';
+import SettingsPage from '../SettingsPage/SettingsPage';
 
-const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD'];
+const colors = {
+  Easy: ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD'],
+  Moderate: ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD', '#B7D968'],
+  Difficult: ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD', '#B7D968', '#555E7B']
+};
 
 class App extends Component {
   constructor() {
     super();
-    this.state = this.getInitialState();
+    this.state = {...this.getInitialState(), difficulty: 'Easy'};
   }
 
   getInitialState() {
     return {
       selColorIdx: 0,
       guesses: [this.getNewGuess()],
-      code: this.genCode()
+      code: this.genCode(),
+      // new state coming in!
+      elapsedTime: 0,
+      isTiming: true
     };
   }
 
@@ -32,7 +38,9 @@ class App extends Component {
   }
 
   genCode() {
-    return new Array(4).fill().map(dummy => Math.floor(Math.random() * 4));
+    let numColors = this.state && colors[this.state.difficulty].length;
+    numColors = numColors || 4;
+    return new Array(4).fill().map(dummy => Math.floor(Math.random() * numColors));
   }
 
   getWinTries() {
@@ -41,6 +49,15 @@ class App extends Component {
     return this.state.guesses[lastGuess].score.perfect === 4 ? lastGuess + 1 : 0;
   }
 
+  handleTimerUpdate = () => {
+    this.setState((curState) => ({elapsedTime: ++curState.elapsedTime}));
+  }
+
+  handleDifficultyChange = (level) => {
+    // Use callback to ensure level is updated BEFORE calling handleNewGameClick
+    this.setState({difficulty: level}, () => this.handleNewGameClick());
+  }
+  
   handleColorSelection = (colorIdx) => {
     this.setState({selColorIdx: colorIdx});
   }
@@ -113,49 +130,49 @@ class App extends Component {
     let guessCopy = {...guessesCopy[currentGuessIdx]};
     let scoreCopy = {...guessCopy.score};
 
-    // Set scores
     scoreCopy.perfect = perfect;
     scoreCopy.almost = almost;
-
-    // Update the NEW guess with the NEW score object
     guessCopy.score = scoreCopy;
-
-    // Update the NEW guesses with the NEW guess object
     guessesCopy[currentGuessIdx] = guessCopy;
 
-    // Add a new guess if not a winner
     if (perfect !== 4) guessesCopy.push(this.getNewGuess());
 
-    // Finally, update the state with the NEW guesses array
     this.setState({
-      guesses: guessesCopy
+      guesses: guessesCopy,
+      // This is a great way to update isTiming
+      isTiming: perfect !== 4
     });
   }
 
   render() {
     let winTries = this.getWinTries();
     return (
-      <div className="App">
-        <header className='App-header-footer'>R E A C T &nbsp;&nbsp;&nbsp;  M A S T E R M I N D</header>
+      <div>
+        <header className='header-footer'>R E A C T &nbsp;&nbsp;&nbsp;  M A S T E R M I N D</header>
         <Switch>
-        <Route exact path='/' render={() =>
-          <GamePage
-            winTries={winTries}
-            colors={colors}
-            selColorIdx={this.state.selColorIdx}
-            guesses={this.state.guesses}
-            handleColorSelection={this.handleColorSelection}
-            handleNewGameClick={this.handleNewGameClick}
-            handlePegClick={this.handlePegClick}
-            handleScoreClick={this.handleScoreClick}
-          />
-        } />
-        <Route exact path='/settings' render={(props) =>
-          <SettingsPage {...props}/>
-        }
-        />
-          
-        
+          <Route exact path='/' render={() =>
+            <GamePage
+              winTries={winTries}
+              colors={colors[this.state.difficulty]}
+              selColorIdx={this.state.selColorIdx}
+              guesses={this.state.guesses}
+              elapsedTime={this.state.elapsedTime}
+              isTiming={this.state.isTiming}
+              handleColorSelection={this.handleColorSelection}
+              handleNewGameClick={this.handleNewGameClick}
+              handlePegClick={this.handlePegClick}
+              handleScoreClick={this.handleScoreClick}
+              handleTimerUpdate={this.handleTimerUpdate}
+            />
+          } />
+          <Route exact path='/settings' render={props => 
+            <SettingsPage
+              {...props} 
+              colorsLookup={colors}
+              difficulty={this.state.difficulty}
+              handleDifficultyChange={this.handleDifficultyChange}
+            />
+          } />
         </Switch>
       </div>
     );
--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import GameBoard from '../../components/GameBoard/GameBoard';
 import ColorPicker from '../../components/ColorPicker/ColorPicker';
 import GameTimer from '../../components/GameTimer/GameTimer';
 import NewGameButton from '../../components/NewGameButton/NewGameButton';
-import { Link } from 'react-router-dom';
 import './GamePage.css';
 
-const GamePage = props => {
+const GamePage = (props) => {
   return (
-    <div className="App">
+    <div className="GamePage">
       <div className="flex-h align-flex-end">
         <GameBoard
           colors={props.colors}
@@ -16,18 +16,22 @@ const GamePage = props => {
           handlePegClick={props.handlePegClick}
           handleScoreClick={props.handleScoreClick}
         />
-        <div className='App-controls'>
+        <div className='GamePage-controls'>
           <ColorPicker
             colors={props.colors}
             selColorIdx={props.selColorIdx}
             handleColorSelection={props.handleColorSelection}
           />
-          <GameTimer />
-          <Link className='btn GamePage-link-margin btn-default' to='/settings'>Difficulty</Link>
+          <GameTimer
+            elapsedTime={props.elapsedTime}
+            handleTimerUpdate={props.handleTimerUpdate}
+            isTiming={props.isTiming}
+          />
+          <Link className='btn btn-default GamePage-link-margin' to='/settings'>Difficulty</Link>
           <NewGameButton handleNewGameClick={props.handleNewGameClick}/>
         </div>
       </div>
-      <footer className='App-header-footer'>
+      <footer className='header-footer'>
         {(props.winTries ? `You Won in ${props.winTries} Guesses!` : 'Good Luck!')}
       </footer>
     </div>
