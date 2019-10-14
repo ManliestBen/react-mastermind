896cc6232e91c27fad3b172c22bc83456b87d0ff
ManliestBen <benjamintmanley@gmail.com>
2019-10-14 17:38:34 -0500

    Updates

---
+++ precache-manifest.580e8fd72fc7e4301a0b11a968b5a11c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7a91df3f3ff31720d1eaf6a55b76a4",
+    "url": "/react-mastermind/index.html"
+  },
+  {
+    "revision": "7b2a7a8765dd2d778289",
+    "url": "/react-mastermind/static/css/main.3f99c518.chunk.css"
+  },
+  {
+    "revision": "284fc3d344f713fdb6e0",
+    "url": "/react-mastermind/static/js/2.e3cd9784.chunk.js"
+  },
+  {
+    "revision": "7b2a7a8765dd2d778289",
+    "url": "/react-mastermind/static/js/main.c6078338.chunk.js"
+  },
+  {
+    "revision": "6b7cbcf201aa2e62d20d",
+    "url": "/react-mastermind/static/js/runtime-main.2e048b5d.js"
+  }
+]);
