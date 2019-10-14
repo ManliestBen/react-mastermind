a01c97df7790748f447df373f98eedcbc56ecf68
ManliestBen <benjamintmanley@gmail.com>
2019-10-14 17:51:43 -0500

    fix for deployment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://sei:${process.env.DATABASE_URL}@sei-1hogc.gcp.mongodb.net/react-mastermind?retryWrites=true&w=majority`, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
